4adcb4d1b20230046a014c4cca63634a46829424
Mauricio Soto <mausotog@hotmail.com>
2015-09-22 16:41:35 -0400

    Silly small changes

--- src/rhythmKeyPackage/PersistentDataStorage.java
+++ src/rhythmKeyPackage/PersistentDataStorage.java
@@ -13,6 +13,6 @@ public class PersistentDataStorage {
 	}
 	
 	public int getCurrentSessionId(){
-		return 0; //Chris: Change this for the method that looks at the number on the last line of the file
+		return 0; //Chris: Change this for the method that looks at the number on the last line of the file, yoou can assume userName is filled out with the user name.
 	}
 }
